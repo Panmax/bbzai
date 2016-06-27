1d579cc47c487adf426739682d6c0f6ea14f2199
Panmax <jiapan.china@gmail.com>
2016-06-27 12:07:53 +0800

    init leancloud

--- bbzai.py
+++ bbzai.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+import leancloud
+
+leancloud.init("3searoQGvzcFv2N1S4mj0yKU-gzGzoHsz", "TYSYkoSOHab4uPqfV7Af7uRg")
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
